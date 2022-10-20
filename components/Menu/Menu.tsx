e0a3bea6529e692ee8855327be58cfb6800f02bb
Jack Lima <jacklimavas@gmail.com>
2022-10-20 10:05:08 -0300

    Deploy

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -14,6 +14,16 @@ const MenuContainer = styled.nav`
         padding: 0;
         display: flex;
         flex-direction: column;
+        height: 250px;
+        overflow-Y: auto;
+        &::-webkit-scrollbar-thumb {
+            background: #242424;
+            outline: 1px solid #777777;
+            border-radius: 2px;
+        }
+        &::-webkit-scrollbar {
+            width: 6px;
+        }
          li {
             font-weight: 400;
             font-size: 1.2rem;
@@ -26,7 +36,7 @@ const MenuContainer = styled.nav`
             border-bottom-right-radius: 5px;
             &:hover {
                 background: #888;
-                border-left-color: #fffb19; 
+                border-left-color: #fffb19;
                 cursor: pointer;
             }
         }
--- components/SideBar/SideBar.tsx
+++ components/SideBar/SideBar.tsx
@@ -1,19 +1,24 @@
 import React from 'react'
-import styled from '@emotion/styled'
-import Profile from '../Profile'
-import Balance from '../Balance'
+import styled from '@emotion/styled';
+import Profile from '../Profile';
+import Balance from '../Balance';
+import Menu from '../Menu';
 
 const SideBarContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 360px;
-  `
+    display: flex;
+    flex-direction: column;
+    width: 450px;
+    height: 100vh;
+    background: #242424;
+    position: fixed;
+`
+
 export default function SideBar() {
   return (
     <SideBarContainer>
-        <Profile name ="Jack Lima" role="Software Engineer" agency={"0001"} accountNumber={"1234567"} />
-        <Balance amount={1924300} />
+        <Profile name="Jack Lima" role="Software Developer" agency="0001" accountNumber="123456789" />
+        <Balance amount={190345} />
+        <Menu />
     </SideBarContainer>
-  
   )
 }
